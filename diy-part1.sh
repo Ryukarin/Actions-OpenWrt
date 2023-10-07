53b7bf27d0a8d130d5efb865c7f951eb27208503
Ryukarin <59430251+Ryukarin@users.noreply.github.com>
2023-10-07 16:39:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#稳定版↓
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+#每日同步更新版↓
+#echo 'src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
